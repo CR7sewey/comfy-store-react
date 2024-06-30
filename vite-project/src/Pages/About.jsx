4e77f4a1f826ec0fcc100f158c8431078db82b92
CR7sewey <miguelmotacarvalho@gmail.com>
2024-06-30 09:52:00 +0100

    About Page correction

--- vite-project/src/Pages/About.jsx
+++ vite-project/src/Pages/About.jsx
@@ -1,22 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
-    <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
-      <h1 className="text-4xl font-bold leading-none tracking-tight">
-        We love
-      </h1>
-      <div className="stats bg-primary shadow">
-        <div className="stat">
-          <div className="stat-title text-primary-content text-4xl font-bold tracking-widest capitalize">
-            comfy
+    <>
+      <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+        <h1 className="text-4xl font-bold leading-none tracking-tight">
+          We love
+        </h1>
+        <div className="stats bg-primary shadow">
+          <div className="stat">
+            <div className="stat-title text-primary-content text-4xl font-bold tracking-widest capitalize">
+              comfy
+            </div>
           </div>
-          <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto ">
-            Hi there. We are not just a store. We are the store.
-          </p>
         </div>
       </div>
-    </div>
+      <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto ">
+        Hi there. We are not just a store. We are the store. So right now go to
+        the <Link to="/products">Products</Link> page and just be happy!
+      </p>
+    </>
   );
 };
 
